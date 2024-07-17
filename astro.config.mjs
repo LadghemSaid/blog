4e5464211a36cdf272a01f9877f63b7b62599685
Said LEDGHEM (STE ATECNA) <sladghem@norauto.com>
2024-07-17 23:49:33 +0200

    chore: Update branch name in deploy workflow to master

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   site: 'https://LadghemSaid.github.io',
-  base: '/blog',
+  base: 'blog',
   output: 'static',
 
   integrations: [
